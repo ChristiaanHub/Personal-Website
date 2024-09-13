03804766c80fe580b291683f235a5697d5939788
Christiaan van Vuuren <christiaanVanVuuren05@gmail.com>
2024-09-13 18:34:13 +0400

    Updated icons

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,21 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path
+      fill="#00bd7e"
+      fill-opacity="1"
+      d="M0,32L40,32C80,32,160,32,240,48C320,64,400,96,480,90.7C560,85,640,43,720,58.7C800,75,880,149,960,149.3C1040,149,1120,75,1200,58.7C1280,43,1360,85,1400,106.7L1440,128L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+    ></path>
+  </svg>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo-transparent-with-name.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
